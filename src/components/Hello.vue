378269a2875ea29d95fed9b1e4a087f6e939388b
SunoAries <476252630@qq.com>
2017-02-07 16:59:36 +0800

    comment2

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -9,7 +9,7 @@ export default {
   name: 'hello',
   data () {
     return {
-      msg: 'Welcome to XIAOCHENGLIN\'s Vue.js App /n  still love xuan'
+      msg: 'Welcome to XIAOCHENGLIN\'s Vue.js App /n  still love xuan  tt'
     }
   }
 }
